7bc615b8af88e5191f3e3bd3a0092d0d4f32c564
lopushok9 <y.g.tsygankov@gmail.com>
2025-12-11 21:33:11 +0300

    website

---
+++ landing-page/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Routes for the specific pages
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/terms', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'terms.html'));
+});
+
+app.get('/how-to-use', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'how-to-use.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
