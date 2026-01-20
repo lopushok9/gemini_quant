974554c79bdeb0ebb6b544f2d2fec29f44c608b7
lopushok9 <y.g.tsygankov@gmail.com>
2026-01-20 15:18:53 +0300

    debug26

--- quanty/src/bus.ts
+++ quanty/src/bus.ts
@@ -2,7 +2,18 @@ import { EventEmitter } from 'events';
 
 class InternalMessageBus extends EventEmitter { }
 
-const internalMessageBus = new InternalMessageBus();
-internalMessageBus.setMaxListeners(50);
+// Используем globalThis чтобы гарантировать один экземпляр bus
+// между агентом (dist/index.js) и сервером (start-server.ts)
+const GLOBAL_BUS_KEY = '__quanty_internal_message_bus__';
+
+let internalMessageBus: InternalMessageBus;
+
+if ((globalThis as any)[GLOBAL_BUS_KEY]) {
+    internalMessageBus = (globalThis as any)[GLOBAL_BUS_KEY];
+} else {
+    internalMessageBus = new InternalMessageBus();
+    internalMessageBus.setMaxListeners(50);
+    (globalThis as any)[GLOBAL_BUS_KEY] = internalMessageBus;
+}
 
 export default internalMessageBus;
